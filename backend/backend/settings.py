552e415e6dd33a6ab649331ed74b08a4db36331f
Jun <jcheong@tailoredindustry.com>
2019-07-01 18:05:18 -0400

    added the API app to settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
 ]
 
 REST_FRAMEWORK = {
