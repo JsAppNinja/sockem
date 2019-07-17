ea7045a9637c751170be56463740508349e13d26
Jun <jcheong@tailoredindustry.com>
2019-07-17 18:02:20 -0400

    POST and other http requests can be made on the User model using token based authentication

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,7 +3,7 @@ Class-based API views using the Django REST Framework
 """
 from rest_framework import generics
 from rest_framework import permissions
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication, BasicAuthentication
 from .models import User
 from .serializers import UserSerializer
 
@@ -12,7 +12,7 @@ class UserList(generics.ListCreateAPIView):
     """
     List all users, or create a new user.
     """
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    authentication_classes = (TokenAuthentication, SessionAuthentication, BasicAuthentication)
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -22,7 +22,7 @@ class UserDetail(generics.RetrieveUpdateAPIView):
     """
     Retrieve, update or delete a user.
     """
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    authentication_classes = (TokenAuthentication, SessionAuthentication, BasicAuthentication)
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
