61ea92e743fd60bff45066c22030e8d38715adf0
Connor Barrett <connor.barrett.dev@gmail.com>
2019-08-30 15:04:22 -0400

    set up basic connection of Redux to React

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux;'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducer from "./store/reducer";
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <BrowserRouter>
